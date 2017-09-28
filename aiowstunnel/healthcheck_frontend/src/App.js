385e76758a0c0e68e9d9c310cb1ac9c0bd305aff
Richard Bann <richardbann@gmail.com>
2017-09-28 07:43:46 +0200

    frontend reconnect

--- aiowstunnel/healthcheck_frontend/src/App.js
+++ aiowstunnel/healthcheck_frontend/src/App.js
@@ -5,20 +5,67 @@ import React, {Component} from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.ws = null;
+    this.nextConnIdx = 0;
+    this.nextConnAt = null;
+    this.timer = null;
+
     this.state = {
-      ws: null,
+      wsState: 'CONNECTING', // CONNECTED, FAILEDRECONNECT, CLOSEDRECONNECT
+      nextConnSec: null,
       data: null
     };
   }
 
-  componentDidMount() {
-    this.ws = new WebSocket('wss://localhost/stats');
-    this.ws.onopen = () => this.setState({ws: this.ws});
-    this.ws.onerror = e => this.setState({ws: null, data: null});
-    this.ws.onclose = e => this.setState({ws: null, data: null});
+  startReconnect() {
+    this.nextConnAt =
+      new Date().getTime() + 1000 * this.props.delays[this.nextConnIdx];
+    this.nextConnIdx =
+      this.nextConnIdx === this.props.delays.length - 1
+        ? this.nextConnIdx
+        : this.nextConnIdx + 1;
+    this.timer = setTimeout(() => this.checkReconnect(), 100);
+  }
+
+  checkReconnect() {
+    const nextConnSec = this.nextConnSec();
+    if (nextConnSec === 0) {
+      this.wsConnect();
+    } else {
+      this.timer = setTimeout(() => this.checkReconnect(), 300);
+      this.setState({
+        nextConnSec: nextConnSec
+      });
+    }
+  }
+
+  nextConnSec() {
+    return Math.floor((this.nextConnAt - new Date().getTime()) / 1000);
+  }
+
+  wsConnect() {
+    this.ws = new WebSocket(this.props.wsURI);
+    this.ws.onopen = () => {
+      this.nextConnIdx = 0;
+      this.setState({
+        wsState: 'CONNECTED',
+        data: null
+      });
+    };
+    this.ws.onclose = () => {
+      this.startReconnect();
+      this.setState({
+        wsState: 'CLOSEDRECONNECT',
+        nextConnSec: this.nextConnSec()
+      });
+    };
     this.ws.onmessage = e => {
       this.setState({data: JSON.parse(e.data)});
     };
+  }
+
+  componentDidMount() {
+    this.wsConnect();
   }
 
   connectionRows(conns) {
@@ -57,22 +104,51 @@ class App extends Component {
   }
 
   render() {
-    if (!this.state.ws || !this.state.data) {
-      return <div className="server">data not available</div>;
-    } else {
-      return (
-        <div>
+    switch (this.state.wsState) {
+      case 'CONNECTING':
+        return <div className="server">connecting...</div>;
+      case 'FAILEDRECONNECT':
+        return (
           <div className="server">
-            tunnel server listening on {this.state.data.host}:{' '}
-            {this.state.data.port}
+            connection failed, reconnecting in {this.state.nextConnSec}{' '}
+            seconds...
           </div>
-          <table cellSpacing="0">
-            <tbody>{this.connectionRows(this.state.data.connections)}</tbody>
-          </table>
-        </div>
-      );
+        );
+      case 'CLOSEDRECONNECT':
+        return (
+          <div className="server">
+            connection closed, reconnecting in {this.state.nextConnSec}{' '}
+            seconds...
+          </div>
+        );
+      case 'CONNECTED':
+        if (this.state.data === null)
+          return <div className="server">connected, waiting for data...</div>;
+        else
+          return (
+            <div>
+              <div className="server">
+                tunnel server listening on {this.state.data.host}:{' '}
+                {this.state.data.port}
+              </div>
+              <table cellSpacing="0">
+                <tbody>
+                  {this.connectionRows(this.state.data.connections)}
+                </tbody>
+              </table>
+            </div>
+          );
+      default:
+        return (
+          <div className="server">invalid state: {this.state.wsState}</div>
+        );
     }
   }
 }
+
+App.defaultProps = {
+  wsURI: 'wss://localhost/stats',
+  delays: [5, 5, 5, 10]
+};
 
 export default App;
